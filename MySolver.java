2842e0bd56048cfe764b6dfa21ed49db97fc702d
AmitSaar1994 <39333634+AmitSaar1994@users.noreply.github.com>
2018-06-12 17:20:55 +0300

    Create MySolver.java

---
+++ MySolver.java
@@ -0,0 +1,11 @@
+package Solver;
+
+public class MySolver implements Solver{
+
+	@Override
+	public String solveProblem(String problem) {
+		// TODO Auto-generated method stub
+		return problem;
+	}
+
+}
